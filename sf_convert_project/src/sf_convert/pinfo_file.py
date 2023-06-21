1b20ecfc118dda9f1dbbc527ce4d0dd92e5bbbea
VIVEK REDDY CHITHARI <65945306+Vectron-IITM@users.noreply.github.com>
2023-06-21 13:08:08 -0400

    layout updated

--- sf_convert_project/src/sf_convert/pinfo_file.py
+++
@@ -1,6 +0,0 @@
-def pinfo(str, default=0):
-    if default == 0:
-        print(str)
-    else:
-        # do something
-        pass
---
+++ sf_convert_project/src/sf_convert/utils/pinfo_file.py
@@ -0,0 +1,6 @@
+def pinfo(str, default=0):
+    if default == 0:
+        print(str)
+    else:
+        # do something
+        pass
