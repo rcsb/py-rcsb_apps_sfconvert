6ff7a90aca67c53121a8fc9b00b351aa6a4fa6ef
Ezra Peisach <ezra.peisach@rcsb.org>
2024-03-20 07:42:16 -0400

    More checks

---
+++ sf_convert_project/src/tests/test_text.py
@@ -0,0 +1,11 @@
+from sf_convert.utils.TextUtils import is_cif
+from sf_convert.utils.pinfo_file import PInfoLogger
+
+
+class TestTextUtils:
+    def test_iscif(self, cif_5pny_coordinate_pdb_path, cif_5pny_coordinate_path):
+        """Tests functionality of is_cif for coordinate files"""
+        logger = PInfoLogger('path_to_log1.log', 'path_to_log2.log')
+
+        assert is_cif(cif_5pny_coordinate_pdb_path, logger) is False
+        assert is_cif(cif_5pny_coordinate_path, logger) is True
